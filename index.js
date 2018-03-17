d25313ecf792ad3e7353c83bff057f2550d0ca9f
Jorge <jacandrade@gmail.com>
2018-03-17 12:57:42 -0300

    implements complete auth flow with separate env keys

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express');
-
-const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'tere' });
-});
+const cookieSession = require('cookie-session');
+const express = require('express');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const app = express();
+
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 app.listen(PORT);
